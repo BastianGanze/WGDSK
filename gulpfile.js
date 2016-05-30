44cb0e9244064f46ee73aaa3c3bbec3d75e7de8a
Bastian Ganze <mcwhite.gk@gmail.com>
2016-05-31 01:14:57 +0200

    * added sourcemaps :D

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     tsify = require('tsify'),
+    buffer = require('vinyl-buffer'),
+    sourcemaps = require('gulp-sourcemaps');
     source = require('vinyl-source-stream'),
     watchify = require('watchify'),
     paths = {src: "src/", build:"build/", tmp:"tmp/"},
     mainFile = "start.ts",
-    version = "0.0.1",
-    bundle;
+    version = "0.0.1";
 
-var srcCodeBundler = browserify().add(paths.src+mainFile).plugin(tsify),
+var srcCodeBundler = browserify({debug:true}).add(paths.src+mainFile).plugin(tsify),
     watchifyBundler = watchify(browserify().add(paths.src+mainFile).plugin(tsify));
 
 function transpile(bundler){
@@ -16,6 +17,7 @@ function transpile(bundler){
     { return bundler.bundle()
         .on('error', function(error){console.error(error.toString()); })
         .pipe(source('build-'+version+'.js'))
+        .pipe(buffer())
         .pipe(gulp.dest(paths.build));
     }
 }
