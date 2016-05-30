e762eaa745108bce03fc05a465920babf6e60d46
Bastian Ganze <mcwhite.gk@gmail.com>
2016-05-31 01:02:56 +0200

    * build-0.0.1.js actually generating code now..

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,31 @@
 var gulp = require('gulp'),
-    ts = require('gulp-typescript'),
-    
-    paths = {src: "src/", build:"build/"},
-    version = "0.0.1";
+    browserify = require('browserify'),
+    tsify = require('tsify'),
+    source = require('vinyl-source-stream'),
+    watchify = require('watchify'),
+    paths = {src: "src/", build:"build/", tmp:"tmp/"},
+    mainFile = "start.ts",
+    version = "0.0.1",
+    bundle;
 
-gulp.task('transpile', function() {
-    return gulp.src(paths.src+"**/*.ts")
-        .pipe(ts({
-            noImplicitAny: true,
-            out: 'build-'+version+'.js'
-        }))
+var srcCodeBundler = browserify().add(paths.src+mainFile).plugin(tsify),
+    watchifyBundler = watchify(browserify().add(paths.src+mainFile).plugin(tsify));
+
+function transpile(bundler){
+    return function()
+    { return bundler.bundle()
+        .on('error', function(error){console.error(error.toString()); })
+        .pipe(source('build-'+version+'.js'))
         .pipe(gulp.dest(paths.build));
-});
+    }
+}
+
+gulp.task('transpile', transpile(srcCodeBundler));
+
+gulp.task('watchify', transpile(watchifyBundler));
 
 gulp.task('watch', ['transpile'], function() {
-    gulp.watch(paths.src+'**/*.ts', ['transpile']);
+    gulp.watch(paths.src+'**/*.ts', ['watchify']);
 });
 
 gulp.task('default', ['transpile']);
